03ed924a5ce22e0ca4fbff32dc7b826691fe1d7c
Dennis Schad <dschad@comwrap.com>
2019-05-16 14:33:50 +0200

    new errorpage

--- src/main/java/de/schad/mi/webmvc/sichtung/SichtungController.java
+++ src/main/java/de/schad/mi/webmvc/sichtung/SichtungController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
@@ -42,10 +44,7 @@ public class SichtungController {
 		try {
 			found = sichtungen.getList().get(nr);
 		} catch(IndexOutOfBoundsException e) {
-			//throw new SichtungNotFoundException(String.format("Sichtung mit der Nummer %s konnte nicht gefunden werden", nr));
-
-			//lieber wieder exception schmeißen
-			found = sichtungen.getList().get(0);
+			throw new SichtungNotFoundException(String.format("Sichtung mit der Nummer %s konnte nicht gefunden werden", nr));
 		}
 
 		m.addAttribute("sichtungform", found);
@@ -75,6 +74,7 @@ public class SichtungController {
 	}
 
 	@ExceptionHandler(SichtungNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public String handleSichtungNotFoundError() {
 		return "error";
 	}
