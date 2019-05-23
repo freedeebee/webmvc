4b6fef650c7824b58ea39aa159335d2fb958efc9
Dennis Schad <dschad@comwrap.com>
2019-05-24 00:35:19 +0200

    fixed frontend and added entity saving of new users

--- src/main/java/de/schad/mi/webmvc/users/User.java
+++ src/main/java/de/schad/mi/webmvc/users/User.java
@@ -1,8 +1,10 @@
 package de.schad.mi.webmvc.users;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -10,11 +12,18 @@ import javax.validation.constraints.Size;
 public class User {
 
     @Id
-    @Size(min = 3)
+    @Size(min = 3, message = "Minimum 3 Characters")
+    @Column(unique = true)
     private String loginname;
+
+    @NotBlank(message = "This field may not be empty")
     private String password;
+
+    @NotBlank(message = "This field may not be empty")
     private String fullname;
     private boolean active;
+
+    public User() {}
 
     public User(String loginname, String password, String fullname) {
         this.loginname = loginname;
@@ -65,5 +74,5 @@ public class User {
         return String.format("loginname: %s, password: %s, fullname: %s, active: %s",
             this.loginname, this.password, this.fullname, this.active);
     }
-    
+
 }
--- src/main/java/de/schad/mi/webmvc/users/UsersController.java
+++ src/main/java/de/schad/mi/webmvc/users/UsersController.java
@@ -1,9 +1,14 @@
 package de.schad.mi.webmvc.users;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,24 +19,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UsersController {
 
-    private Logger logger = org.slf4j.LoggerFactory.getLogger(UsersController.class);
+    private Logger logger = LoggerFactory.getLogger(UsersController.class);
+
+    private final UserRepository repository;
+
+    public UsersController(UserRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping
-    public String getUserDashboard() {
+    public String getUserDashboard(Model m) {
+        m.addAttribute("users", repository.findAll());
         return "userdashboard";
     }
 
     @GetMapping("/create")
-    public String getNewUserForm() {
+    public String getNewUserForm(Model m) {
+        m.addAttribute("userform", new User());
         return "userform";
     }
-    
+
 
     @PostMapping("/create")
     public String newUser(
-            @ModelAttribute("userform") User user,
-            BindingResult result) {
-        logger.info(user.toString());
+            @Valid @ModelAttribute("userform") User user,
+            BindingResult result,
+            Model m) {
+
+        if(result.hasErrors()) {
+            return "userform";
+        }
+
+        Optional<User> databaseUser = repository.findById(user.getLoginname());
+        if (databaseUser != null) {
+            m.addAttribute("usernametaken", "Username is already taken");
+            return "userform";
+        }
+
+        logger.info(user.toString() + "saved into Database");
+        repository.save(user);
+
         return "redirect:/users";
     }
 
