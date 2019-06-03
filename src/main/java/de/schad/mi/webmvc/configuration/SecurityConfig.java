0341d928f8e42eed39d4c4d0ac63b65fd37a9807
Dennis Schad <dennis.schad1991@gmail.com>
2019-06-03 12:17:48 +0200

    Spring Security

---
+++ src/main/java/de/schad/mi/webmvc/configuration/SecurityConfig.java
@@ -0,0 +1,40 @@
+package de.schad.mi.webmvc.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder amb) throws Exception {
+
+        PasswordEncoder encoder = getPasswordEncoder();
+
+        amb.inMemoryAuthentication()
+            .withUser("admin")
+            .password(encoder.encode("geheim"))
+            .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/sichtung")
+            .permitAll()
+            .anyRequest().hasRole("ADMIN");
+    }
+    
+}
